e0e301b9d8372a6c806cbda183a2c49975dcd53b
gabigoncalves <38844334+gabigoncalves@users.noreply.github.com>
2018-05-01 13:06:12 -0300

    Update lab-01.py

--- Exercicios/lab-01.py
+++ Exercicios/lab-01.py
@@ -1,9 +1,7 @@
 //Faça um programa que leia dois valores e exiba o maior valor lido.
 
-print ("Escolha um número:")
-n1 = input ()
-print ("Agora escolha outro número:")
-n2 = input ()
+n1 = input ("Escolha um número:")
+n2 = input ("Agora escolha outro número:")
 if (n1>n2):
   print ("O maior valor lido foi:", n1)
 elif (n2>n1):
