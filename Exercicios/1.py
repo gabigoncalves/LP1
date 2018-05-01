fd4d004ad264680999afae433f4fe00c44605ebc
gabigoncalves <38844334+gabigoncalves@users.noreply.github.com>
2018-05-01 14:00:48 -0300

    Update 1.py

--- Exercicios/1.py
+++ Exercicios/1.py
@@ -1,10 +1,10 @@
-//Faça um programa que leia dois valores e exiba o maior valor lido.
+#Ler dois números n1 e n2 e exibir todos os números entre n1 e n2.
 
 n1 = input ("Escolha um número:")
 n2 = input ("Agora escolha outro número:")
 if (n1>n2):
-  print ("O maior valor lido foi:", n1)
-elif (n2>n1):
-  print ("O maior valor lido foi:", n2)
-else:
-  (print "Os dois valores são iguais:", n1)
+    for i in range (n1,n2):
+    print ("Os números entre",n1," e ",n2," são:",i)
+else
+    for i in range (n2,n1):
+    print ("Os números entre",n2," e ",n1," são:",i)
