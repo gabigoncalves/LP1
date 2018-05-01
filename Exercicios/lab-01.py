6d79b4d09644060331fc99150dc9245dfcba447e
gabigoncalves <38844334+gabigoncalves@users.noreply.github.com>
2018-05-01 12:49:39 -0300

    Update and rename lab-01 to lab-01.py

---
+++ Exercicios/lab-01.py
@@ -0,0 +1,11 @@
+//1 - Faça um programa que leia dois valores e exiba o maior valor lido.
+print ("Escolha um número:")
+n1 = input ()
+print ("Agora escolha outro número:")
+n2 = input ()
+if (n1>n2):
+  print ("O maior valor lido foi:", n1)
+elif (n2>n1):
+  print ("O maior valor lido foi:", n2)
+else:
+  (print "Os dois valores são iguais:", n1)
