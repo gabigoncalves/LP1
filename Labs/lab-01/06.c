0527fa721227b6e715062cc9d1fef74d17c6131c
gabigoncalves <38844334+gabigoncalves@users.noreply.github.com>
2018-05-04 12:26:27 -0300

    Update 06.c

--- Labs/lab-01/06.c
+++ Labs/lab-01/06.c
@@ -3,15 +3,18 @@
 #include <stdio.h>
 
 int main () {
-    int n;
-    int i;
-    int soma;
-    printf ("Escreva um número: ");
-    scanf ("%d",&n);
-    soma = 0;
-    for (i=1; i<=n; i++) {
-        soma = soma + i*i;
-        printf ("O somatório é: %d\n",soma);
-    }
-    return 0;
-} 
+	int n;
+	int soma;
+	int i;
+	printf ("Escreva um número: ");
+	scanf ("%d",&n);	
+	soma = 0;
+	i = 1;
+
+	for (i; i <= n; i++) 
+	{
+		soma = soma + (i*i);
+	}
+	printf ("O somatório é: %d\n",soma);
+	return 0;
+}
