eea86dde40abbb17c89fd744c1ee78498bc71e60
gabigoncalves <38844334+gabigoncalves@users.noreply.github.com>
2018-05-03 23:56:34 -0300

    Create 06.c

---
+++ Labs/lab-01/06.c
@@ -0,0 +1,16 @@
+//06.c
+
+#include <stdio.h>
+int main () {
+    int n;
+    int i = n**2;
+    int soma;
+    printf ("Escreva um número: ");
+    scanf ("%d",n);
+    soma = i;
+    for x in range (1,n+1) {
+        soma = soma + i;
+        printf ("O somatório é: %d\n",soma);
+    }
+    retun 0;
+}    
