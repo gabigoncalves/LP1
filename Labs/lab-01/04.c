69c15ca3169d77fa6a28f893c40b93b02247a181
gabigoncalves <38844334+gabigoncalves@users.noreply.github.com>
2018-05-03 23:35:34 -0300

    Update 04.c

--- Labs/lab-01/04.c
+++ Labs/lab-01/04.c
@@ -5,16 +5,17 @@ int main () {
 	float velocidade;
 	float kph;
 	float mph;
-	printf ("Qual a velocidade do carro em mph? ");
+	printf ("Qual a velocidade do carro em mph? \n");
 	scanf ("%f",&mph);
 	velocidade = mph*1.6;
 	printf ("A velocidade do carro em Km/h é: %.2f\n", velocidade);	
 	if (80<=velocidade && velocidade<=100) {
-		printf ("MANTENHA.");
+		printf ("MANTENHA.\n");
 	} else if (velocidade < 80) { 
-		printf ("ACELERE.");
+		printf ("ACELERE.\n");
 	} else {
-		printf ("DESACELERE.");
+		printf ("DESACELERE.\n");
 	}
 	return 0;
 }
+
