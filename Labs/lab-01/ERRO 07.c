f0d7117c99ca557b3fd3cf13d1c98a78073282e8
gabigoncalves <38844334+gabigoncalves@users.noreply.github.com>
2018-05-05 16:49:44 -0300

    Update ERRO 07.c

--- Labs/lab-01/ERRO 07.c
+++ Labs/lab-01/ERRO 07.c
@@ -4,22 +4,18 @@
 #include <stdio.h>
 
 int main () {
-	int times = 20;
-	int vit = 3;
-	int emp = 1;
-	int der = 0;
 	int num_vit = 0;
 	int num_emp = 0;
 	int num_der = 0;
-	int i = 0;
 	int pontos;
-	int menos_pontos = 0;
-	int mais_pontos = 1000;
+	int i;
+	int menos_pontos = 1000;
+	int mais_pontos = 0;
 	char nome;
 	char nome_pior;
 	char nome_melhor;
 
-	for (i; i < times; i++) {
+	for (i=0; i < 3; i++) {
 		printf ("Digite o nome do time: ");
 		scanf ("%c", &nome);
 		printf ("Digite o número de vitórias desse time: ");
@@ -28,21 +24,19 @@ int main () {
 		scanf ("%d", &num_emp);
 		printf ("Digite o número de derrotas: ");
 		scanf ("%d", &num_der);
-		pontos = (num_vit*vit) + (num_emp*emp) + der;
-	
-		if  (pontos > mais_pontos) {
-			mais_pontos = pontos;
-			nome_melhor = nome;
-		}
-		else if (pontos < menos_pontos) {
-			nome_melhor = nome;
-			menos_pontos = pontos;
-		}
+		pontos = (num_vit*3) + (num_emp*1);
+	}
+	if  (pontos > mais_pontos) {
+		mais_pontos = pontos;
+		nome_melhor = nome;
+	}
+	else if (pontos < menos_pontos) {
+		menos_pontos = pontos;
+		nome_pior = nome;
 	}
 	printf ("O time com menos pontos é: %c\n",nome_pior);
 	printf ("A quantidade de pontos foi: %d\n",menos_pontos);
 	printf ("O time com mais pontos é: %c\n",nome_melhor);
 	printf ("A quantidade de pontos foi: %d\n",mais_pontos);
-	i = i + 1;
 	return 0;
 }
