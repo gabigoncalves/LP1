d3ccb646c8cac0c0f81ce2de8a99f5745c9a99d7
gabigoncalves <38844334+gabigoncalves@users.noreply.github.com>
2018-05-06 18:38:53 -0300

    Update 03.c

--- Labs/lab-01/03.c
+++ Labs/lab-01/03.c
@@ -5,15 +5,14 @@ int main () {
 	int din = 1000;	
 	float preco;
 	float total;
-	printf ("Digite o preço do celular em dólares:");
+	printf ("Digite o preço do celular em dólares: ");
 	scanf ("%f",&preco);
 	total = preco/3.17; 
 	if (total <= din) {
-		printf ("Bom negócio! O valor do celular em reais é: %f\n", total);
+		printf ("Bom negócio! O valor do celular é: R$%.2f\n", total);
  	}	
 	else {
-		printf ("Não é um bom negócio... O valor do celular em reais é: %f\n", total);
+		printf ("Não é um bom negócio... O valor do celular é: R$%.2f\n", total);
 	}
 	return 0;
 }
-
