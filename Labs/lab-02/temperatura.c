e067a970276357e8a91a1ec6ba3516058cfda969
gabigoncalves <38844334+gabigoncalves@users.noreply.github.com>
2018-05-08 22:25:44 -0300

    Update temperatura.c

--- Labs/lab-02/temperatura.c
+++ Labs/lab-02/temperatura.c
@@ -2,24 +2,24 @@
 
 #include <stdio.h>
 int main () {
-	char temp[5];
+	char temp[50];
 	int t;	
 	int i;
 	int soma_temp = 0;
 	int media;
 	int soma_media = 0;
 
-	for (i=0; i< 5; i++) {
+	for (i=0; i< 50; i++) {
 		printf ("Insira a temperatura: \n");
 		scanf ("%d", &t);
 		temp[i] = t;
 		soma_temp = soma_temp + temp[i];
 	}
 	printf ("A soma das temperaturas é: %d\n", soma_temp);	
-	media = soma_temp/5;
+	media = soma_temp/50;
 	printf ("A média das temperaturas é: %d\n", media);
 
-	for (i=0; i< 5; i++) {
+	for (i=0; i< 50; i++) {
 		if (temp[i] > media) {
 			printf ("As temperaturas acima da média são: %d\n", temp[i]);
 			soma_media = soma_media + 1;
