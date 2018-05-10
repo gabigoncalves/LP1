75b16affcbb35f64536c0ec7366ee65920764029
gabigoncalves <38844334+gabigoncalves@users.noreply.github.com>
2018-05-09 22:30:08 -0300

    Create eh_primo.c

---
+++ Labs/lab-02/eh_primo.c
@@ -0,0 +1,28 @@
+//eh_primo.c
+
+#include <stdio.h>
+
+int eh_primo (int n) {
+	int i;
+	for (i=2;i<n;i++) {
+		if (n%i == 0) {
+			return (0);
+		}
+	}
+return (1);
+}
+
+int main () {
+	int n;
+	printf ("Digite um número: ");
+	scanf ("%d",&n);
+	
+	if (eh_primo(n) == 0) {
+		printf ("Não é primo! \n");
+	}
+	else {
+		printf ("É primo! \n");
+	}
+	return 0;
+}
+	
