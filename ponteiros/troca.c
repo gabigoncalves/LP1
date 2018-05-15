ab3627aaea795b9bd2d62b342cab11f7a8e49d23
gabigoncalves <38844334+gabigoncalves@users.noreply.github.com>
2018-05-14 22:52:47 -0300

    Create troca.c

---
+++ ponteiros/troca.c
@@ -0,0 +1,22 @@
+//troca.c
+
+#include <stdio.h>
+
+void troca (int* p1, int* p2) {
+	int tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;  
+}
+
+int main () { 
+	int x;
+	int y;
+	printf ("Digite um valor p1: ");
+	scanf ("%d", &x);
+	printf ("Digite um valor p2: ");
+	scanf ("%d", &y);
+	troca(&x,&y);
+	printf("%d %d\n", x, y);
+  
+  return 0;
+}
