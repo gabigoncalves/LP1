a75f6d7ed7cb904ac45f069a4fa8256ac34f9080
gabigoncalves <38844334+gabigoncalves@users.noreply.github.com>
2018-05-24 19:43:38 -0300

    Update ex7-1.c

--- composto/struct/ex7-1.c
+++ composto/struct/ex7-1.c
@@ -7,7 +7,7 @@ struct BancodeDados {
 	char seu_nome[10];
 	char nome_hotel[20];
 	int gasto;
-	int pontuacao;
+	float pontuacao;
 };
 
 void preenche (struct BancodeDados* bd) {
@@ -18,12 +18,12 @@ void preenche (struct BancodeDados* bd) {
 	printf ("Insira o gasto aproximado de sua viagem: R$");
 	scanf ("%d",&((*bd).gasto));
 	printf ("De 0 a 5, qual a sua satisfação? ");
-	scanf ("%d",&((*bd).pontuacao));
+	scanf ("%f",&((*bd).pontuacao));
 }
 
 int main (void) {
 	struct BancodeDados bd;
 	preenche (&bd);
-	printf ("\nSeu nome: %s\nHotel: %s\nGasto: R$%d\nPontuação: %d\n\n", bd.seu_nome, bd.nome_hotel, bd.gasto, bd.pontuacao);
+	printf ("\nSeu nome: %s\nHotel: %s\nGasto: R$%d\nPontuação: %.1f\n\n", bd.seu_nome, bd.nome_hotel, bd.gasto, bd.pontuacao);
 	return 0;
 }
