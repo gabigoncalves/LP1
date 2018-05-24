a6916396486b7aebf56329950ec17e64f2123c17
gabigoncalves <38844334+gabigoncalves@users.noreply.github.com>
2018-05-24 13:11:20 -0300

    Create ex7-1.c

---
+++ composto/struct/ex7-1.c
@@ -0,0 +1,29 @@
+//Exercício 7.1
+
+#include <stdio.h>
+#include <string.h>
+
+struct BancodeDados {
+	char seu_nome[10];
+	char nome_hotel[20];
+	int gasto;
+	int pontuacao;
+};
+
+void preenche (struct BancodeDados* bd) {
+	printf ("Digite seu primeiro nome: ");
+	scanf ("%s",(*bd).seu_nome);
+	printf ("Insira o nome do hotel em que você se hospedou: ");
+	scanf ("%s",(*bd).nome_hotel);
+	printf ("Insira o gasto aproximado de sua viagem: R$");
+	scanf ("%d",&((*bd).gasto));
+	printf ("De 0 a 5, qual a sua satisfação? ");
+	scanf ("%d",&((*bd).pontuacao));
+}
+
+int main (void) {
+	struct BancodeDados bd;
+	preenche (&bd);
+	printf ("\nSeu nome: %s\nHotel: %s\nGasto: R$%d\nPontuação: %d\n\n", bd.seu_nome, bd.nome_hotel, bd.gasto, bd.pontuacao);
+	return 0;
+}
