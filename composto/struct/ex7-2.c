24f07f097b8afe7aa43a19a0b9979dab41944123
gabigoncalves <38844334+gabigoncalves@users.noreply.github.com>
2018-05-29 18:22:35 -0300

    Update ex7-2.c

--- composto/struct/ex7-2.c
+++ composto/struct/ex7-2.c
@@ -14,34 +14,25 @@ struct Numeros {
 	struct Nomes nome;
 };
 
-void preenche (struct Nomes* nome) {
+void preenche2 (struct Numeros* n) {
 	printf ("Digite seu primeiro nome: ");
-	scanf ("%s",(*nome).seu_nome);
+	scanf ("%s",(*n).nome.seu_nome);
 	printf ("Insira o nome do hotel em que você se hospedou: ");
-	scanf ("%s",(*nome).nome_hotel);
-}
-
-void preenche2 (struct Numeros* num) {
+	scanf ("%s",(*n).nome.nome_hotel);
 	printf ("Insira o gasto aproximado de sua viagem: R$");
-	scanf ("%d",&((*num).gasto));
+	scanf ("%d",&((*n).gasto));
 	printf ("De 0 a 5, qual a sua satisfação? ");
-	scanf ("%d",&((*num).pontuacao));
+	scanf ("%d",&((*n).pontuacao));
 }
 
 int main (void) {
-	struct Nomes nome;
-	preenche(&nome);
-	struct Numeros num;
-	preenche2(&num);
-	strcpy( num.nome.seu_nome, "Luiz Miguel");
-	strcpy( num.nome.nome_hotel, "Copacabana Palace");
-	num.gasto = 5000;	
-	num.pontuacao = 5;
+	struct Numeros n;
+	preenche2(&n);
 
-	printf( "\nSeu nome: %s\n", num.nome.seu_nome);
-	printf( "Nome do Hotel: %s\n", num.nome.nome_hotel);
-	printf( "Gasto: %d\n", num.gasto);
-	printf( "Pontuaçao: %d\n\n", num.pontuacao);
+	printf( "\nSeu nome: %s\n", n.nome.seu_nome);
+	printf( "Nome do Hotel: %s\n", n.nome.nome_hotel);
+	printf( "Gasto: R$%d\n",n.gasto);
+	printf( "Pontuaçao: %d\n\n", n.pontuacao);
 	
 	return 0;
 }
