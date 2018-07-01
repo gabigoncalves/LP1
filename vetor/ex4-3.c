29276e7f1e80b2b91346372370164bea491d23a0
gabigoncalves <38844334+gabigoncalves@users.noreply.github.com>
2018-06-30 23:35:17 -0300

    Update ex4-3.c

--- vetor/ex4-3.c
+++ vetor/ex4-3.c
@@ -6,31 +6,47 @@ struct Ponto {
 	int y;
 };
 
-void preenche_ponto (struct Ponto* p) {
+void preenche_ponto (struct Ponto* ponto) {
 	printf ("Digite o ponto x: ");
-	scanf ("%d", &(*p).x);
+	scanf ("%d", &(*ponto).x);
 	printf ("Digite o ponto y: ");
-	scanf ("%d", &(*p).y);
+	scanf ("%d", &(*ponto).y);
 }
 
-void preenche_vetor (struct Ponto* ps, int n) {
+void preenche_vetor (struct Ponto ps[]) {
 	int i;
-	for (i=0; i<n; i++) {
+	for (i=0; i<9; i++) {
 		struct Ponto p;
 		preenche_ponto(&p);
 		ps[i] = p;
-		printf ("Ponto: (%d,%d)\n", p.x, p.y);	
 	}	
 }
 
-void ponto_distante () {
-
-	//distancia = sqrt(pow((xb-xa),2)+pow((yb-ya),2));
+struct Ponto ponto_distante (struct Ponto ps[10]) {
+	int i;
+	float maior = 0;
+	float distancia;
+	struct Ponto maior_ponto;
+	for (i=0; i<9; i++) {
+		distancia = sqrt(pow(ps[i].x - 0,2) + pow(ps[i].y - 0,2));
+		if (distancia > maior) {
+			maior_ponto.x = ps[i].x;
+			maior_ponto.y = ps[i].y;
+			maior = distancia; 
+		}
+	}
+	return maior_ponto;
 }
 
 int main (void) {
+	int i;
+	struct Ponto maior_ponto;
 	struct Ponto ps[10];
 	preenche_vetor(ps);
-	struct Ponto p = ponto_distante(ps);
+	
+	for (i=0; i<9; i++) {
+		maior_ponto = ponto_distante(ps);
+	}
+	printf ("\nPonto mais distante de (0,0) é: (%d,%d)\n", maior_ponto.x, maior_ponto.y); 
 	return 0;
 }
